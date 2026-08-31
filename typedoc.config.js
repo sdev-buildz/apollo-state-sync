edafe05a259a6776604ad33ff27d31003336fa0e
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-08-31 22:53:18 +0530

    fix(typedoc.config): added workspace entry points. removed a plugin temporarily.

--- typedoc.config.js
+++ typedoc.config.js
@@ -2,10 +2,10 @@ import packageJson from './package.json' with { type: 'json' }
 
 /** @type {Partial<import("typedoc").TypeDocOptions>} */
 export default {
-  name: `${packageJson?.name ?? 'canonical-serialization.'} - API Reference (Users)`,
-  entryPoints: ['src', 'src/for-worker-thread'],
+  name: `${packageJson?.name ?? 'apollo-state-sync.'} - API Reference (Users)`,
+  entryPoints: ['src', 'packages/apollo-shared-ws/src'],
   out: 'docs/generated/api-reference',
   exclude: ['dist', 'docs', '**/generated', 'node_modules'],
   skipErrorChecking: true,
-  plugin: ['@packages/typedoc-plugins/link-tag-plugin'],
+  // plugin: ['@packages/typedoc-plugins/link-tag-plugin'],
 }
