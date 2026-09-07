dc9e9616668ded90a3e1808ac8ddfc585feb9e5a
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-07 23:07:34 +0530

    refactor(build-graphql): move generate .graphql sdl logic into @packages/web-app-utils for SSoT.

---
+++ e2e/web-app/buildGql.ts
@@ -0,0 +1,12 @@
+import { buildGraphQL, getCodegenConfig } from '@packages/web-app-utils'
+import sharedConfig from '@shared/config'
+import path from 'node:path'
+import { schema } from './server/schema'
+
+const tsEmitFilePath = `${import.meta.dirname}/generated/typescript-react-apollo.tsx`
+
+buildGraphQL(
+  schema,
+  path.join(import.meta.dirname, sharedConfig.graphqlSdlPath),
+  getCodegenConfig(tsEmitFilePath, schema)
+)
