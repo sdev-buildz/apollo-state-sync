774c18fab34105919fb53ccae78fa7f6bad73b95
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-07 19:59:51 +0530

    refactor(webpack): moved webpack config and devServer logics into @packages/web-app-utils internal package for SSoT. sharedConfig is also added to the internal package.

---
+++ packages/web-app-utils/src/node/webpack.start-server.ts
@@ -0,0 +1,58 @@
+/**
+ * @packageDocumentation
+ * {@inheritdoc startWebpackServer}
+ */
+import { logger } from '@packages/logger'
+import webpack from 'webpack'
+import WebpackDevServer from 'webpack-dev-server'
+import { getWebpackConfig } from './webpack.config'
+const config = getWebpackConfig()
+
+/**
+ *  Runs the development server with hot module reload.
+ *  When process.env.CI=true, hot module reload will be disabled.
+ */
+export const startWebpackServer = () => {
+  const compiler = webpack(config)
+
+  if (!compiler) {
+    logger.error(
+      'Ensure if you are passing any callback to the webpack function'
+    )
+    process.exit(1)
+  }
+
+  if (!config.devServer) {
+    logger.error(
+      'Ensure the devServer field is not undefined in the webpack configuration object.'
+    )
+    process.exit(1)
+  }
+
+  const server = new WebpackDevServer(config.devServer, compiler)
+
+  const runServer = async () => {
+    logger.info('Starting the frontend dev server...')
+    await server.start()
+  }
+
+  runServer().catch((error) => {
+    logger.error('Failed to start the frontend dev server:', error)
+    logger.error('Failed to start the frontend dev server:')
+
+    process.exit(1)
+  })
+
+  /**
+   * Gracefully shuts down the frontend dev server.
+   */
+  async function gracefullyShutdown() {
+    logger.info('\nGracefully shutting down the frontend dev server...')
+    await server.stop()
+    process.exit(0)
+  }
+
+  process.on('SIGINT', gracefullyShutdown)
+
+  process.on('SIGTERM', gracefullyShutdown)
+}
