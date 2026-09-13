a31528eaa99fe917599bfae53e4371b708cb4120
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-13 18:27:16 +0530

    refactor(winston yoga): move winston yoga logger into a dedicated lib file

--- e2e/web-app/server/util/yogaServerInstance.ts
+++ e2e/web-app/server/util/yogaServerInstance.ts
@@ -1,6 +1,6 @@
-import { logger } from '@packages/logger'
 import { createYoga } from 'graphql-yoga'
 import { type GraphqlContextType } from '../lib/types'
+import { winstonYogaLogger } from '../lib/winstonYogaLogger'
 import { schema } from '../schema'
 import { initGraphqlContext } from './initGraphqlContext'
 
@@ -15,17 +15,5 @@ export const yoga = createYoga<GraphqlContextType>({
   graphiql: {
     subscriptionsProtocol: 'WS',
   },
-  logging: {
-    debug: (...args) => logger.debug(args),
-    error: (...args) => {
-      if (
-        args[0]?.path?.[0] === 'queryForError' ||
-        args[0]?.path?.[0] === 'subscribeForError'
-      )
-        return
-      logger.error(args)
-    },
-    info: (...args) => logger.info(args),
-    warn: (...args) => logger.warn(args),
-  },
+  logging: winstonYogaLogger,
 })
