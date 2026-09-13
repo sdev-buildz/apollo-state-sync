953969eb15faffcab52f0f0dc8eed073851a7fc1
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-13 16:56:21 +0530

    refactor(server config): move web-app server config into @packages/web-app-utils for SSoT

--- e2e/web-app/server/config.ts
+++ e2e/web-app/server/config.ts
@@ -3,37 +3,6 @@
  * @see config
  */
 
-import { logger } from '@packages/logger'
-import { sharedConfig } from '@packages/web-app-utils/shared'
+import { serverConfig } from '@packages/web-app-utils'
 
-/**
- * The server's configuration object.
- * It abstracts the environment variables.
- */
-const config = {
-  sampleEnv: process.env.SAMPLE_ENV,
-  ...sharedConfig,
-
-  auth: {
-    /**
-     * Used in authorization header. The key must be 32 bytes (256 bits) for AES-256
-     */
-    jweKey:
-      process.env.JWE_KEY ??
-      (sharedConfig.environment === 'DEV' ? '1'.repeat(32) : ''),
-  },
-}
-
-if (!config.auth.jweKey) {
-  logger.error(
-    `No JWE Key present. Provide a JWE_KEY environment variable and keep it secure from others.`,
-    {
-      readMoreAboutJwe: `https://datatracker.ietf.org/doc/html/rfc7516`,
-    }
-  )
-  process.emit('SIGTERM')
-}
-
-config.auth.jweKey = config.auth.jweKey as string
-
-export default config
+export { serverConfig as config }
--- packages/web-app-utils/src/node/index.ts
+++ packages/web-app-utils/src/node/index.ts
@@ -1,3 +1,4 @@
 export * from './graphql-builder'
+export * from './serverConfig'
 export * from './webpack.config'
 export * from './webpack.start-server'
