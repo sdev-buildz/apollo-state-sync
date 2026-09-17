0274587f352b1363a65f4598c27ffbe331002d19
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-17 17:19:52 +0530

    docs(shopping site): add dark mode. improve saturation and transparency of content-based colors

--- examples/shopping-site/web-client/lib/colors.ts
+++ examples/shopping-site/web-client/lib/colors.ts
@@ -1,41 +1,41 @@
-import { getPaletteSync, getSwatchesSync } from 'colorthief'
-
-/**
- * @returns lighter version of color
- */
-function lightenColor(rgbString: string, percent: number = 90 / 100): string {
-  const rgb = rgbString.match(/\d+/g)!.map(Number)
-  const r = Math.round(rgb[0]! + (255 - rgb[0]!) * percent)
-  const g = Math.round(rgb[1]! + (255 - rgb[1]!) * percent)
-  const b = Math.round(rgb[2]! + (255 - rgb[2]!) * percent)
-  return `rgb(${r}, ${g}, ${b})`
-}
+import {
+  getPaletteSync,
+  getSwatchesSync,
+  type Color,
+  type HSL,
+} from 'colorthief'
 
 /**
  *  Gets {@link https://m3.material.io/styles/color/dynamic/content-based-source | Content-based Color} from image.
  */
 export const getImgColor = (img: HTMLImageElement): string => {
-  const swatches = getSwatchesSync(img)
-  let choosenColor: string | undefined = undefined
+  const swatches = getSwatchesSync(img, { colorSpace: 'oklch' })
+  let choosenColor: Color | undefined = undefined
 
   //  Choose vibrant color if present
   if (swatches.Vibrant) {
-    choosenColor = swatches.Vibrant.color.css()
+    choosenColor = swatches.Vibrant.color
   }
   if (!choosenColor) {
-    const palette = getPaletteSync(img)
+    const palette = getPaletteSync(img, { colorSpace: 'oklch' })
 
     //  Choose a non-grey color
     for (const color of palette!) {
-      const rgb = color.rgb()
-      const rgbArr = [rgb.r, rgb.g, rgb.b]
-      if (Math.max(...rgbArr) - Math.min(...rgbArr) <= 20) continue
-      choosenColor = color.css()!
+      // const rgb = color.rgb()
+      const hsl = color.hsl()
+      if (hsl.s < 10) continue
+      choosenColor = color
       break
     }
 
     // If no vibrant or non-grey colors are present, choose a random color
-    if (!choosenColor) choosenColor = palette![0]?.css() ?? ''
+    if (!choosenColor) {
+      choosenColor = palette?.[0]
+    }
   }
-  return lightenColor(choosenColor)
+
+  const hsl: HSL = choosenColor?.hsl() ?? { h: 20, s: 100, l: 50 }
+  hsl.l = 50
+
+  return `hsl( ${hsl.h} ${hsl.s}% ${hsl.l}% / 18% )`
 }
