ea0a7bdd91ce6d8f67f2a2d044b7e7195190440d
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-18 23:19:13 +0530

    docs(shopping site): add login feature

--- examples/shopping-site/web-client/contexts/AppContext.ts
+++ examples/shopping-site/web-client/contexts/AppContext.ts
@@ -1,17 +1,20 @@
+import { makeVarSynced, type ReactiveVarSynced } from 'apollo-state-sync'
 import { createContext } from 'react'
 
 /**
  * Type for the {@link AppContext}
  */
 export interface AppContextType {
-  isLoggedIn?: boolean
+  isLoggedIn: ReactiveVarSynced<boolean>
 }
 
 /**
  * To initiate the AppContext with dummy values.
  * These values are only to initialize and not meant to be used by the consuming components.
  */
-const defaultAppContext: AppContextType = {}
+export const defaultAppContext: AppContextType = {
+  isLoggedIn: makeVarSynced(false, 'isLoggedIn'),
+}
 
 /**
  * The AppContext is used to provide global context variables.
