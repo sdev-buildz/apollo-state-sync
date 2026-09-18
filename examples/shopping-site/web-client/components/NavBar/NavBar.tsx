303e1dbc49c01d456ec242db97d3f53529c4c77f
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-18 23:19:13 +0530

    docs(shopping site): add login feature

--- examples/shopping-site/web-client/components/NavBar/NavBar.tsx
+++ examples/shopping-site/web-client/components/NavBar/NavBar.tsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useReactiveVar } from '@apollo/client/react'
+import { useContext } from 'react'
+import { AppContext } from '../../contexts/AppContext'
 
 /**
  * The top NavBar component.
  */
 export const NavBar = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const { isLoggedIn } = useContext(AppContext)
+  const isLoggedInState = useReactiveVar(isLoggedIn)
 
   return (
     <nav id='nav-bar'>
       <div className='login-box'>
-        {isLoggedIn ? 'Logged In' : 'Logged Out'}
-        <button type='button' onClick={(prev) => setIsLoggedIn(!prev)}>
-          {isLoggedIn ? 'Log out' : 'Log In'}
+        {isLoggedInState ? 'Logged In' : 'Logged Out'}
+        <button type='button' onClick={() => isLoggedIn(!isLoggedIn())}>
+          {isLoggedInState ? 'Log out' : 'Log In'}
         </button>
       </div>
     </nav>
