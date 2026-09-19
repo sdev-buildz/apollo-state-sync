cd32a053082d8cd82db7e3f0e40691459ec94756
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-19 15:36:26 +0530

    Revert "fix: try full process.env"
    This reverts commit 4542c5be17937cd8365215fa8848d650b07fcf74.

--- packages/web-app-utils/src/node/webpack.config.ts
+++ packages/web-app-utils/src/node/webpack.config.ts
@@ -151,10 +151,10 @@ export const getWebpackConfig = (
         template: './public/index.html',
       }),
 
+      /** Providing a safe subset of process.env as a global variable. */
       new webpack.DefinePlugin({
         process: {
-          // env: JSON.stringify(publicEnv),
-          env: JSON.stringify(process.env),
+          env: JSON.stringify(publicEnv),
         },
         'globalThis.__DEV__': true,
       }),
