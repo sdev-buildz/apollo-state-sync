9a7bb961d4145603d795ecd5d3afb1a91c779b50
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-25 16:38:52 +0530

    docs(social media site): add json-ld describing Apollo State Sync

--- examples/social-media/web-client/HtmlHeadChildren.tsx
+++ examples/social-media/web-client/HtmlHeadChildren.tsx
@@ -1,5 +1,5 @@
 import { sharedConfig } from '@packages/web-app-utils/shared'
-
+import type { SoftwareSourceCode, WithContext } from 'schema-dts'
 /**
  * The children of the head tag such as Script tags, Link tags, etc...
  */
@@ -20,6 +20,61 @@ export const HtmlHeadChildren = () => {
         type='text/css'
       />
       <script src='https://apis.google.com/js/api:client.js'></script>
+      <script
+        type='application/ld+json'
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'SoftwareSourceCode',
+            name: 'Apollo State Sync',
+            description:
+              'A TypeScript npm library that synchronizes and persists Apollo Client state, including the InMemoryCache and reactive variables, seamlessly across different browsing contexts (browser tabs, windows, iframes).',
+            codeRepository:
+              'https://github.com/sdev-buildz/apollo-state-sync.git',
+            runtimePlatform: 'Web Browsers, Node.js',
+            programmingLanguage: {
+              '@type': 'ComputerLanguage',
+              name: 'TypeScript',
+              alternateName: 'TS',
+              url: 'https://www.typescriptlang.org/',
+            },
+            keywords:
+              'apollo-client, state-management, sync, cross-tab, reactive-variables, cache, persist, npm, typescript',
+            license:
+              'https://github.com/sdev-buildz/apollo-state-sync/blob/main/LICENSE',
+            maintainer: {
+              '@type': 'Person',
+              '@id': 'https://github.com/sdev-buildz',
+              name: 'sdev-buildz',
+              url: 'https://github.com/sdev-buildz',
+              email: 'stevexdev@zohomail.in',
+            },
+            targetProduct: {
+              '@type': 'SoftwareApplication',
+              applicationCategory: 'DeveloperApplication',
+              name: 'apollo-state-sync',
+              softwareHelp: {
+                '@id':
+                  'https://github.com/sdev-buildz/apollo-state-sync.git#readme',
+              },
+              installUrl: 'https://www.npmjs.com/package/apollo-state-sync',
+              downloadUrl: 'https://registry.npmjs.org/apollo-state-sync',
+              supportingData: [
+                {
+                  '@type': 'DataFeed',
+                  name: 'Apollo Client Cache',
+                  url: 'https://www.apollographql.com/docs/react/caching/overview',
+                },
+                {
+                  '@type': 'DataFeed',
+                  name: 'Reactive Variables',
+                  url: 'https://www.apollographql.com/docs/react/v3/local-state/reactive-variables',
+                },
+              ],
+            },
+          } satisfies WithContext<SoftwareSourceCode>),
+        }}
+      />
     </>
   )
 }
