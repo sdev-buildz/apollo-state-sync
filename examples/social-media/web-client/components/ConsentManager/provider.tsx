3f9d77c2dd6aaba531222257949b67f8d41e5d97
sdev-buildz <310740165+sdev-buildz@users.noreply.github.com>
2026-09-25 23:24:24 +0530

    docs(social media site): add privacy policy banner

---
+++ examples/social-media/web-client/components/ConsentManager/provider.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import {
+  ConsentBanner,
+  ConsentDialog,
+  ConsentManagerProvider,
+} from '@c15t/react'
+import { type ReactNode } from 'react'
+
+/**
+ * Consent management context provider.
+ */
+export default function ConsentManagerClient({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <ConsentManagerProvider
+      options={{
+        mode: 'offline',
+        consentCategories: ['necessary', 'measurement', 'marketing'],
+      }}
+    >
+      <ConsentBanner />
+      <ConsentDialog />
+      {children}
+    </ConsentManagerProvider>
+  )
+}
